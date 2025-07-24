68f866b7cefd7f82fd8db7715811281ef05c5676
lkdtt <Julius.luo2019@outlook.com>
2025-07-24 00:51:40 -0400

    specified files type

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Gallery from './pages/Gallery';
+import Navbar from './components/Navbar.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Projects from './pages/Projects.jsx';
+import Gallery from './pages/Gallery.jsx';
 
 function App() {
   return (
